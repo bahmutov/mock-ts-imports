061e868e017a923c6c6089355e274c998774ff37
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-05-20 10:10:57 -0400

    restoring mocks

--- user-spec.ts
+++ user-spec.ts
@@ -1,9 +1,11 @@
 // "compute" imports "add" from "./math" using named import
-import test from 'ava'
+import test, { beforeEach } from 'ava'
 import { compute } from './user'
 import { ImportMock } from 'ts-mock-imports'
 // to mock "./math add" export need to import entire module
 import * as math from './math'
+
+beforeEach(ImportMock.restore)
 
 test('real add', t => {
   // no stubbing
@@ -14,4 +16,8 @@ test('stubbed add', t => {
   // somehow stub "add" from "./math.ts" to return known value like 100
   ImportMock.mockFunction(math, 'add', 100)
   t.deepEqual(compute(2, 3), 100)
+})
+
+test('add stays stubbed', t => {
+  t.deepEqual(compute(-1, 7), 6)
 })
